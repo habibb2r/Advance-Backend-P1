702f4f8e6c92edc448e2d45969894db3bc973888
habibb2r <habibhk127@gmail.com>
2024-12-06 01:10:53 +0600

    update with user validation

--- src/Modules/User/user.validation.ts
+++ src/Modules/User/user.validation.ts
@@ -1,12 +1,11 @@
 import { z } from 'zod'
 
 const userValidationSchema = z.object({
-    id: z.string().max(30, { message: 'Password must be less than 30 characters'}),
-    password: z.string().max(20, { message: 'Password must be less than 20 characters'}),
-    needsPasswordChange: z.boolean().optional().default(true),
-    role: z.enum(['admin', 'student', 'faculty']),
-    status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-    isDeleted: z.boolean().optional().default(false),
+    
+    password: z.string({
+        invalid_type_error: 'Password must be string'
+    }).max(20, { message: 'Password must be less than 20 characters'}).optional(),
+  
 })
 
 export const UserValidation ={
